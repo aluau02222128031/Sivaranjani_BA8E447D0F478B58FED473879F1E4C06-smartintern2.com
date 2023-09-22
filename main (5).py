5e39aab375d844d8b0c6caf70952296be95d86d7
aluau02222128031 <144785827+aluau02222128031@users.noreply.github.com>
2023-09-22 13:18:06 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,20 @@
+#Define the base class player
+class Player:
+  def play(self):
+    print("The player is playing cricket")
+  #define the derived class Batsman
+class Batsman(Player):
+  def play(self):
+    print("the batsman is batting")
+#define the derived class bowler
+class Bowler(Player):
+    def play(self):
+      print("the bowler is bowling")
+      
+batsman=Batsman()
+bowler=Bowler() 
+player=Player()
+
+batsman.play()
+bowler.play()
+player.play()
